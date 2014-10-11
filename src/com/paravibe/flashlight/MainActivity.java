cfced06bc853e4b67b88759d592dd13dfd496a5b
Max Petyurenko <max@drucode.com>
2014-10-11 15:32:21 +0300

    Refactoring.

--- src/com/paravibe/flashlight/MainActivity.java
+++ src/com/paravibe/flashlight/MainActivity.java
@@ -1,95 +1,121 @@
 package com.paravibe.flashlight;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
-import android.os.Handler;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
   private Camera camera;
-  private boolean isFlashOn = false;
-  private boolean isLedOn = false;
-  private boolean hasFlash;
-  private Handler mHandler = new Handler();
-  private int strobeIntensity = 10;
-  ImageButton btnSwitch;
-  SeekBar strobeIntensitySeek;
-  Parameters params;
+  private Camera.Parameters params;
+  private boolean isLedOn;
+  private boolean hasLed;
+  private static ImageView imageView;
+  private static ImageView toggleButton;
+  private static Drawable flashOnImage;
+  private static Drawable flashOffImage;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-
-    btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
-    strobeIntensitySeek = (SeekBar) findViewById(R.id.seekBar);
 
     // First check if device is supporting flashlight or not.
-    hasFlash = getApplicationContext().getPackageManager()
+    hasLed = getApplicationContext().getPackageManager()
         .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
-    if (!hasFlash) {
-      // Device doesn't support flash.
+    if (!hasLed) {
       // Show alert message and close the application.
-      AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-      builder.setTitle("Error")
-      .setMessage("Sorry, your device doesn't support flash light!")
-      .setNegativeButton("Close", new DialogInterface.OnClickListener() {
-        public void onClick(DialogInterface dialog, int which) {
-          // Closing the application.
-          finish();
-        }
-      });
-      AlertDialog alert = builder.create();
-      alert.show();
+      new AlertDialog.Builder(MainActivity.this)
+          .setTitle("Error")
+          .setMessage("Sorry, your device doesn't support flash light!")
+          .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+              finish();
+            }
+          })
+          .show();
+
       return;
     }
 
-    // Switch button click event to toggle flash on/off.
-    btnSwitch.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) {
-        int strobeValue = strobeIntensitySeek.getProgress();
-        turnFlashOff(isFlashOn);
-        if (strobeValue != 0) {
-          startStrobe();
+    setContentView(R.layout.main);
+
+    imageView = (ImageView) findViewById(R.id.background);
+    toggleButton = (ImageView) findViewById(R.id.powerButton);
+    flashOffImage = getResources().getDrawable(R.drawable.flashlight_off);
+    flashOnImage = getResources().getDrawable(R.drawable.flashlight_on);
+
+    isLedOn = true;
+  }
+
+  public boolean onKeyDown(int i, KeyEvent keyevent) {
+    switch (i) {
+      // key Vol+;
+      case KeyEvent.KEYCODE_VOLUME_UP:
+        setFlashStatus(!isLedOn);
+        break;
+
+      // key Vol-;
+      case KeyEvent.KEYCODE_VOLUME_DOWN:
+        setFlashStatus(true);
+        break;
+
+      default:
+        return super.onKeyDown(i, keyevent);
+    }
+
+    return true;
+  }
+
+  public boolean onKeyUp(int i, KeyEvent keyevent) {
+    switch (i) {
+      // key Vol+;
+      case KeyEvent.KEYCODE_VOLUME_UP:
+        break;
+
+      // key Vol-;
+      case KeyEvent.KEYCODE_VOLUME_DOWN:
+        setFlashStatus(false);
+        break;
+
+      default:
+        return super.onKeyUp(i, keyevent);
+    }
+
+    return true;
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if (hasLed) {
+      camera = null;
+      getCamera();
+      setFlashStatus(isLedOn);
+
+      // Switch button click event to toggle flash on/off.
+      toggleButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+          setFlashStatus(!isLedOn);
         }
-      }
-    });
+      });
+    }
+  }
 
-    strobeIntensitySeek.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-      int progressValue = 0;
-
-      @Override
-      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        progressValue = progress;
-        stopStrobe();
-        if (progressValue == 0) {
-          turnFlashOff(!isFlashOn);
-        }
-        else {
-          startStrobe();
-        }
-      }
-
-      @Override
-      public void onStartTrackingTouch(SeekBar seekBar) {
-
-      }
-
-      @Override
-      public void onStopTrackingTouch(SeekBar seekBar) {
-        strobeIntensity = (progressValue + 1) * 30;
-      }
-    });
+  @Override
+  protected void onStop() {
+    super.onStop();
+    if (camera != null) {
+      camera.release();
+      camera = null;
+    }
   }
 
   // Get the camera
@@ -105,12 +131,15 @@ public class MainActivity extends Activity {
     }
   }
 
-  // Turning on/off flash.
-  private void turnFlashOff(boolean flag) {
-    String mode = Parameters.FLASH_MODE_TORCH;
-    if (flag) {
-      mode = Parameters.FLASH_MODE_OFF;
+  public void setFlashStatus(Boolean status) {
+    String mode = Camera.Parameters.FLASH_MODE_TORCH;
+    isLedOn = true;
+
+    if (!status) {
+      mode = Camera.Parameters.FLASH_MODE_OFF;
+      isLedOn = false;
     }
+    toggleButton(isLedOn);
 
     if (camera == null || params == null) {
       return;
@@ -119,100 +148,14 @@ public class MainActivity extends Activity {
     params.setFlashMode(mode);
     camera.setParameters(params);
     camera.stopPreview();
-
-    isFlashOn = !flag;
-
-    // Changing button/switch image.
-    toggleButtonImage();
   }
 
-  /**
-   * Toggle switch button images.
-   * Changing image states to on/off.
-   */
-  private void toggleButtonImage() {
-    if (isFlashOn) {
-      btnSwitch.setImageResource(R.drawable.button_on);
+  private void toggleButton(boolean status) {
+    if (status) {
+      imageView.setImageDrawable(flashOnImage);
     }
     else {
-      btnSwitch.setImageResource(R.drawable.button_off);
-    }
-  }
-
-  private final Runnable mRunnable = new Runnable() {
-    public void run() {
-      if (isFlashOn) {
-        if (isLedOn) {
-          params.setFlashMode(Parameters.FLASH_MODE_TORCH);
-          camera.setParameters(params);
-        }
-        else {
-          params.setFlashMode(Parameters.FLASH_MODE_OFF);
-          camera.setParameters(params);
-        }
-
-        isLedOn = !isLedOn;
-        mHandler.postDelayed(mRunnable, strobeIntensity);
-      }
-      else {
-        stopStrobe();
-      }
-    }
-  };
-
-  /**
-   * Start flashlight strobe.
-   */
-  private void startStrobe() {
-    mHandler.post(mRunnable);
-  }
-
-  private void stopStrobe() {
-    mHandler.removeCallbacks(mRunnable);
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-
-    stopStrobe();
-    turnFlashOff(true);
-  }
-
-  @Override
-  protected void onRestart() {
-    super.onRestart();
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-
-    // Get the camera params.
-    getCamera();
-    turnFlashOff(false);
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-
-    stopStrobe();
-
-    // Release the camera.
-    if (camera != null) {
-      camera.release();
-      camera = null;
+      imageView.setImageDrawable(flashOffImage);
     }
   }
 }
